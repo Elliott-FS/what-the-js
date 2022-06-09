0607e06ae1bb495f9c623a77f6b6942f7a72f8b9
Elliott Delano Wynn III <delanowynn@yahoo.com>
2022-06-09 09:09:39 -0400

    code push challenge

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const ninSelect = {
+        ninOne: 'Sasuke',
+        ninTwo: 'Naruto',
+        ninThree: 'Sakura',
+    };
+    
+    const ninSummon = {
+       sumOne: 'Snake',
+       sumTwo: 'Toad',
+       sumThree: 'Slug',
+    };
+    
+    const ninJutsu = {
+        'jutsuOne': 'Chidori',
+        'jutsuTwo': 'Rasengan',
+        'jutsuThree': 'Sage Mode',
+    };
+    ninJutsu.forEach(jutsu => {
+   let p = document.createElement('p')
+    console.log(p.textContent = ninJutsu.jutsuOne)
+    }
+    
+     
+})
